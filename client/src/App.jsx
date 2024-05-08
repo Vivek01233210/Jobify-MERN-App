ebb702ed0e57e20bf913e079b8f5a23a115079e5
Vivek Kumar <vivek3553.vr@gmail.com>
2024-05-08 13:06:18 +0530

    removed r-query devtool

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { HomeLayout, DashboardLayout, Landing, Register, Login, Error, AllJobs, Admin, Stats, AddJob, Profile, EditJob } from './pages/index';
 import { action as registerAction } from './pages/Register.jsx';
@@ -103,7 +103,7 @@ const queryClient = new QueryClient({
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       <RouterProvider router={router} />
     </QueryClientProvider>
   )
